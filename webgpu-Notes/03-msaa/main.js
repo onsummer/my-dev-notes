a467b321e837e9026bcb2e9e36e6af78b9334294
onsummer <onsummer@foxmail.com>
2021-04-02 03:16:37 +0800

    [add] webgpu msaa code without readme.

---
+++ webgpu-Notes/03-msaa/main.js
@@ -0,0 +1,131 @@
+/*
+  2021年4月2日 
+*/
+
+const canvas = document.getElementById('gpuweb')
+const vbodata = new Float32Array([
+  -0.5, 0.0, 1.0, 0.0, 0.0, 1.0,
+  0.0, 0.5, 0.0, 1.0, 0.0, 1.0,
+  0.5, 0.0, 0.0, 0.0, 1.0, 1.0
+])
+
+async function render() {
+  if (!navigator.gpu) {
+    alert('你的浏览器不支持 WebGPU 或未开启 WebGPU 支持')
+    return
+  }
+  const adapter = await navigator.gpu.requestAdapter()
+  const device = await adapter.requestDevice()
+
+  const context = canvas.getContext('gpupresent')
+  const swapChainFormat = `bgra8unorm`
+  const swapChain = context.configureSwapChain({
+    device,
+    format: swapChainFormat
+  })
+
+  const sampleTimes = 4
+
+  const vbo = device.createBuffer({
+    size: vbodata.byteLength,
+    usage: GPUBufferUsage.VERTEX,
+    mappedAtCreation: true
+  })
+  new Float32Array(vbo.getMappedRange()).set(vbodata)
+  vbo.unmap()
+
+  const pipeline = device.createRenderPipeline({
+    vertex: {
+      module: device.createShaderModule({
+        code: `
+        [[builtin(position)]] var<out> out_position: vec4<f32>;
+        [[location(0)]] var<out> out_color: vec4<f32>;
+        [[location(0)]] var<in> in_position_2d: vec2<f32>;
+        [[location(1)]] var<in> in_color_rgba: vec4<f32>;
+        [[stage(vertex)]]
+        fn main() -> void {
+          out_position = vec4<f32>(in_position_2d, 0.0, 1.0);
+          out_color = in_color_rgba;
+          return;
+        }
+        `
+      }),
+      entryPoint: 'main',
+      buffers: [{
+        arrayStride: 6 * vbodata.BYTES_PER_ELEMENT,
+        attributes: [{
+          shaderLocation: 0,
+          offset: 0,
+          format: 'float32x2'
+        }, {
+          shaderLocation: 1,
+          offset: 2 * vbodata.BYTES_PER_ELEMENT,
+          format: 'float32x4'
+        }]
+      }]
+    },
+    fragment: {
+      module: device.createShaderModule({
+        code: `
+        [[location(0)]] var<out> outColor: vec4<f32>;
+        [[location(0)]] var<in> in_color: vec4<f32>;
+        [[stage(fragment)]]
+        fn main() -> void {
+          outColor = in_color;
+          return;
+        }
+        `
+      }),
+      entryPoint: 'main',
+      targets: [
+        {
+          format: swapChainFormat
+        }
+      ]
+    },
+    primitive: {
+      topology: 'triangle-list',
+    },
+    multisample: {
+      count: sampleTimes
+    }
+  })
+
+  const texture = device.createTexture({
+    size: {
+      width: canvas.width,
+      height: canvas.height,
+    },
+    sampleCount: sampleTimes,
+    format: swapChainFormat,
+    usage: GPUTextureUsage.RENDER_ATTACHMENT,
+  })
+  const msaa_textureView = texture.createView()
+
+  const commandEncoder = device.createCommandEncoder()
+  const textureView = swapChain.getCurrentTexture().createView()
+  const renderPassDescriptor = {
+    colorAttachments: [
+      {
+        attachment: msaa_textureView,
+        resolveTarget: textureView,
+        loadValue: {
+          r: 0.0,
+          g: 0.0,
+          b: 0.0,
+          a: 1.0
+        }
+      }
+    ]
+  }
+
+  const passEncoder = commandEncoder.beginRenderPass(renderPassDescriptor)
+  passEncoder.setPipeline(pipeline)
+  passEncoder.setVertexBuffer(0, vbo)
+  passEncoder.draw(3, 1, 0, 0)
+  passEncoder.endPass()
+
+  device.queue.submit([commandEncoder.finish()])
+}
+
+render()
--- webgpu-Notes/03-msaa/readme.md
+++ webgpu-Notes/03-msaa/readme.md
@@ -1,1 +1,10 @@
 多重采样抗锯齿
+
+指定采样次数 4 次（其他次数不知道为什么不支持）
+在创建 pipeline 中指定 MultiSampleState
+创建一个 texture，用来重采样
+将 renderPassDescriptor 中 colorAttachments 中的 attachment 设为此 texture 的 view
+将 renderPassDescriptor 中 colorAttachments 中的 resolveTarget 设为交换链的 texture 的 view
+
+重点就是 renderPassDescriptor.colorAttachments.attachment 和 resolveTarget 的设置
+个人理解，resolveTarget 就是最终要输出的，attachment 是中间的（类似于FBO？）
